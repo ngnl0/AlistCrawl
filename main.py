5da5b784be0636184b9e6cbcfa4e559bb7294625
ngnl0 <xialianyunhan@gmail.com>
2022-12-04 15:29:37 +0800

    ✨ feat(main.py config.toml): add config file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import os
+import toml
 import json
 import requests
+from requests import Session
+
 
 size = 0
 
-def spider(data: dict):
-    global size 
-    if(data['data']['type']) == 'file':
+
+def spider(data: dict,url,req_data,session: Session):
+    global size
+    if (data['data']['type']) == 'file':
         file_size = data['data']['files'][0]['size']
         file_name = data['data']['files'][0]['name']
         file_url = data['data']['files'][0]['url']
@@ -15,32 +20,44 @@ def spider(data: dict):
         tmp.pop()
         tmp.pop()
         tmp2 = '/'.join(tmp) + '/'
-        req_data.update({'path':f"{tmp2}"})
+        req_data.update({'path': f"{tmp2}"})
 
     folder_len = data['data']['meta']['total']
-    for _ in range(0,folder_len):
+    for _ in range(0, folder_len):
         folder_path = data['data']['files'][_]['name'] + '/'
         full_path = req_data['path'] + folder_path
-        req_data.update({'path':f"{full_path}"})
-        resp = requests.post(url,data=json.dumps(req_data),headers=headers)
+        req_data.update({'path': f"{full_path}"})
+        resp = session.post(url, data=json.dumps(req_data))
         resp_data = json.loads(resp.text)
-        spider(resp_data)
+        spider(resp_data,url,req_data,session)
         tmp = full_path.split('/')
         tmp.pop()
         tmp.pop()
         tmp = '/'.join(tmp) + '/'
-        req_data.update({'path':f"{tmp}"})
+        req_data.update({'path': f"{tmp}"})
 
 
-req_data = {
-    "path": "/",
-    # path password
-    "password": "*********"
-}
-headers = {'Content-Type': 'application/json'}
+class ReadConfig(object):
+    def __init__(self, conf_path):
+        self.alist_conf = toml.load(conf_path, _dict=dict)['Alist']
+        self.url = self.alist_conf['url'] + '/api/public/path'
+        self.password = self.alist_conf['pass']
+        self.path = self.alist_conf['path']
+        
 
-url = "http://***.***.com/api/public/path"
-resp = requests.post(url,data=json.dumps(req_data),headers=headers)
-resp_data = json.loads(resp.text)
-spider(resp_data)
-print(f"Total files size:{size}")
+    def req_data(self):
+        data = {"path": f"{self.path}", "password": f"{self.password}"}
+        return json.dumps(data)
+
+    def create_session(self):
+        headers = {'Content-Type': 'application/json'}
+        session = requests.Session()
+        session.headers.update(headers)
+        return session
+
+
+if  __name__ == "__main__":
+    conf = ReadConfig('./config.toml')
+    session = conf.create_session()
+    data = json.loads(session.post(url=conf.url,data=conf.req_data()).text)
+    spider(data,conf.url,json.loads(conf.req_data()),session)
